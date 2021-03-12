56e50a98b1b6d02b9973a8a9b71dc5434542c7da
Diogo Rodrigues <diogo.rodrigues@itsector.pt>
2021-03-12 12:20:58 +0000

    commit to manage frontend implementations

--- src/main/java/com/example/javaacademy/controller/AppController.java
+++ src/main/java/com/example/javaacademy/controller/AppController.java
@@ -1,14 +1,39 @@
 package com.example.javaacademy.controller;
 
+import com.example.javaacademy.entity.User;
+import com.example.javaacademy.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+@Controller
 public class AppController {
+
+    @Autowired
+    private UserRepository repository;
 
     @GetMapping("")
     public String viewHomePage () {
         return "index";
     }
+
+
+    @GetMapping("/register")
+    public String showSignUpForm (Model model) {
+        model.addAttribute("user", new User());
+        return "signup_form";
+    }
+
+    @PostMapping("/process_register")
+    public String processRegistration (User user){
+        repository.save(user);
+        return "register_success";
+
+    }
+
+
 }
--- src/main/java/com/example/javaacademy/repository/UserRepository.java
+++ src/main/java/com/example/javaacademy/repository/UserRepository.java
@@ -2,6 +2,12 @@ package com.example.javaacademy.repository;
 
 import com.example.javaacademy.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User  u WHERE u.id = ?1 ")
+    Optional<User> findById (Long id);
 }
